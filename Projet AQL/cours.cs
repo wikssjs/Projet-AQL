61ce168ed3a2963b961b99a1f44e019a739fc2f8
hilali anas <anashilali2004@gmail.com>
2022-08-02 19:09:51 -0400

    classe cours

---
+++ Projet AQL/cours.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_AQL
+{
+
+    internal class Cours
+    {
+        private int _numcours;
+        private string _code;
+        private string _titre;
+
+        public Cours(int numcours, string code, string titre)
+        {
+            Code = code;
+            Titre = titre;
+            Numcours = numcours;
+        }
+
+        public int Numcours { get => _numcours; set => _numcours = value; }
+        public string Code { get => _code; set => _code = value; }
+        public string Titre { get => _titre; set => _titre = value; }
+        public override string ToString()
+        {
+            return $"code : {Code}\n titre : {Titre} \n numcours{Numcours}";
+        }
+    }
+}
