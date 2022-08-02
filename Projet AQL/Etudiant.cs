761f4b36ae726946b658e30551718948dd7d0a65
James <72478345+wikssjs@users.noreply.github.com>
2022-08-02 18:51:11 -0400

    J'ai fait la classe etudiant

--- Projet AQL/Etudiant.cs
+++ Projet AQL/Etudiant.cs
@@ -8,5 +8,24 @@ namespace Projet_AQL
 {
     internal class Etudiant
     {
+        private int _numEtudiant ;
+        private string _nom;
+        private string _prenom;
+
+        public Etudiant(int numEtudiant,string nom,string prenom)
+        {
+            Prenom = prenom;
+            Nom = nom;
+            NumEtudiant = numEtudiant;
+        }
+
+        public int NumEtudiant { get => _numEtudiant; set => _numEtudiant = value; }
+        public string Nom { get => _nom; set => _nom = value; }
+        public string Prenom { get => _prenom; set => _prenom = value; }
+
+        public override string ToString()
+        {
+            return $"Prenom : {Prenom}\n Nom : {Nom} \n NumEtudiant{NumEtudiant}";
+        }
     }
 }
